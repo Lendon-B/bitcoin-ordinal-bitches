0c5db3e1bfc049d80734f39930a4e5f8c76dea8f
Lendon-B <lendonbracewell1114@gmail.com>
2024-09-24 11:27:33 +0200

    add: wallet connect

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,6 @@
 import React, { ReactNode } from "react";
 import { Chain, Network, OrdConnectProvider, OrdConnectKit } from "@ordzaar/ord-connect";
 
-// // import { PageProvider } from "@/contexts/PageContext";
-// // import { SolanaWalletProvider } from "@/contexts/SolanaWalletProvider";
-// import { QueryClientProvider, QueryClient } from "react-query";
-
-// const queryClient = new QueryClient();
-
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <OrdConnectProvider network={Network.TESTNET} chain={Chain.BITCOIN}>
