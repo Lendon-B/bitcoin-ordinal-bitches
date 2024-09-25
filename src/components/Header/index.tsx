d9c46f19eed788e674c9ff7b3432e2521d43c153
leionion <j.ohnceballos0716@gmail.com>
2024-09-25 17:27:54 +0900

    feat: wallet connect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Logo from "../../../public/assest/images/bitches logo.png"
 
 export default function index() {
-  const { setOpenModal } = useContext<any>(UserContext);
+  const { setOpenModal, address } = useContext<any>(UserContext);
   return (
     <div className='w-full h-[90px] flex flex-col bg-black border-b-[2px] border-b-[#411A5B]'>
       <div className='container'>
@@ -13,7 +13,7 @@ export default function index() {
             <Image src={Logo} alt="Logo" className='max-w-[100px] sm:max-w-[250px] h-[50] flex flex-col' />
           </a>
           <div onClick={() => setOpenModal(true)} className='flex flex-col w-[120px] h-[32px] sm:w-[250px] sm:h-[50px] items-center justify-center rounded-full ord-connect-font text-white font-bold text-[14px] sm:text-xl cursor-pointer'>
-            <p>Connect Wallet</p>
+            {address ? <p className='w-[80%] truncate'>{address}</p> : <p>Connect Wallet</p>}
           </div>
         </div>
       </div>
---
+++ src/utils/wallet/okx.ts
@@ -0,0 +1,28 @@
+import {
+    isInstalled,
+    getAddresses,
+    signPsbt,
+    signMessage,
+  } from "@ordzaar/ordit-sdk/okx";
+
+export const connectToOkxWallet = async (): Promise<Array<any>> => {
+    if (!isInstalled()) {
+      throw new Error("Wallet is not installed");
+    }
+  
+    const addresses = await getAddresses("mainnet"); // mainnet or testnet
+
+    return addresses;
+    
+    // Example output of addresses:
+    //
+    // [{
+    //   publicKey:
+    //     "0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798",
+    //   address: "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4",
+    //   format: "segwit",
+    // }]
+    //
+    // or an error is thrown by the wallet provider.
+  }
+  
